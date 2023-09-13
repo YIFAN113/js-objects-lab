27545ae6504d9a8096d7cd252657a3f21014ef0b
YIFAN113 <20095253@mail.wit.ie>
2023-09-13 23:20:16 +0800

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -5,3 +5,43 @@ const aCar = {
   };
   
   console.log(aCar.owner);
+  console.log(aCar.owner + ' drives a ' + aCar.type.make)
+  const bCar = {
+    owner : 'Joe Bloggs',
+    type : {
+        make : 'Toyota',
+        model : 'Corolla',
+        cc : 1.8
+    },
+    registration : '201WD1058'
+};
+console.log(aCar.owner + ' drives a ' + aCar.type.make);
+
+const cCar = {
+  owner: "Joe Bloggs",
+  type: {
+    make: "Toyota",
+    model: "Corolla",
+    cc: 1.8
+  },
+  registration: { year: 201, county: "WD", number: 1058 }
+};
+
+console.log(aCar.owner + " drives a " + aCar.type.make);
+console.log(
+  "Reg. = " +
+    aCar.registration.year +
+    "-" +
+    aCar.registration.county +
+    "-" +
+    aCar.registration.number
+);
+
+const dCar = {
+  owner: "Joe Bloggs",
+  previous_owners: [
+    "Pat Smith - 1 Main Street",
+    "Sheila Dwyer - 2 High Street"
+  ],
+
+};
